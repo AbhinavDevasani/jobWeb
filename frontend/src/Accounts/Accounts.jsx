01d9d11c825bf1abf16322a171f789f9b81a879a
AbhinavDevasani <abhinavdevasani@gmail.com>
2025-08-28 20:12:40 +0530

    Go to login page

--- frontend/src/Accounts/Accounts.jsx
+++ frontend/src/Accounts/Accounts.jsx
@@ -18,10 +18,14 @@ const Accounts = () => {
   const goToListPage=()=>{
     navigate("/jobs")
   }
+  const goToLoginPage=()=>{
+    navigate("/")
+  }
   if (!user) {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <p className="text-gray-700 text-lg">You are not logged in.</p>
+        <button className="bg-blue-500 text-white" onClick={()=>{goToLoginPage()}}>Go to Login Page</button>
       </div>
     );
   }
