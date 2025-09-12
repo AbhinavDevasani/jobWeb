868a158c0e39dd06a7b08c4e7f16575500da3d9a
AbhinavDevasani <abhinavdevasani@gmail.com>
2025-09-12 15:45:19 +0530

    changes in middleware

--- backend/middleWare/tokenHandler.js
+++ backend/middleWare/tokenHandler.js
@@ -13,7 +13,7 @@ export const validateToken = async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_STRING);
 
       // find user and attach
-      req.user = await User.findById(decoded.id).select("-password");
+      req.user = await User.findById(decoded.user._id).select("-password");
 
       if (!req.user) {
         return res.status(401).json({ message: "User not found" });
