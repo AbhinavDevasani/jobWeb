de1989c3b990ad6d4efeb33a1caa70590da7f5b6
AbhinavDevasani <abhinavdevasani@gmail.com>
2025-12-19 16:05:10 +0530

    Refresh error fix

--- frontend/src/ProtectedRoute/index.jsx
+++ frontend/src/ProtectedRoute/index.jsx
@@ -5,7 +5,13 @@ import AuthContext from '../Context/AuthContext';
 
 const ProtectedRoute = ({ children, onlyRahul = false }) => {
   const token = Cookies.get("jwt_token");
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+  
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  
   if (!token) {
     return <Navigate to="/" replace />;
   }
